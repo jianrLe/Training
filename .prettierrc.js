7a270e7b3ec9545aea9c76d51c880037ce8fb652
Jian Le <101063621+jianrLe@users.noreply.github.com>
2022-05-31 16:33:25 +0700

    Setup new branch feature/init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  printWidth: 120,
+  tabWidth: 4,
+};
